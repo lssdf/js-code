2c85df58b43c91aa41b3ec8965736649d999dac6
Harendra Godara <126669597+lssdf@users.noreply.github.com>
2025-08-11 13:50:32 +0000

    datatype

---
+++ javascript/datatype.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as  newer version
+
+// alert("Hello") we are using nodejs not browser
+
+console.log(3 + 3);
+
+let myName = "Harendra"
+let age = 21
+let isLoggedIn = false
+
+console.table([myName, age, isLoggedIn]);
+
+// number
+// bigint
+//string
+// boolean
+// null
+// undefine
+//symbol
+
+
+console.log(typeof"harendra");
+
+console.log(typeof null);
+
+console.log(typeof indefined);
+
+
