88f6bdd76631c193655f2b39ab687178875ca5a7
Harendra Godara <126669597+lssdf@users.noreply.github.com>
2025-08-11 12:03:34 +0000

    container config done

---
+++ javascript/variable.js
@@ -0,0 +1,12 @@
+const accountId = 742498
+let emailId = "harendragodara880@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+
+accountCity = "nagaur"
+let accountState
+// console.log(accountId);
+
+
+console.table([accountId,accountPassword ,emailId , accountCity, accountState])
