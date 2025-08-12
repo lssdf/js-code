de4bf0fd94125e1acc859d86437ff16a4da66dc2
Harendra Godara <126669597+lssdf@users.noreply.github.com>
2025-08-12 18:54:13 +0000

    operation

---
+++ javascript/operation.js
@@ -0,0 +1,18 @@
+let value = 3
+let negValue = -value
+
+console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3);
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+let str1 = "Hello"
+let str2 = " Harendra"
+ 
+let str3 = str1+str2
+
+console.log(str3);
