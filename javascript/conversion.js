1c013b3bf685ed8f3d5915db26219e48d69a1bd9
Harendra Godara <126669597+lssdf@users.noreply.github.com>
2025-08-12 18:38:46 +0000

    conversion

---
+++ javascript/conversion.js
@@ -0,0 +1,32 @@
+// let score = false
+
+// console.log(typeof (score));
+
+// let valueNumber = Number(score)
+
+// console.log(valueNumber);
+
+// console.log(typeof valueNumber);
+
+// "33" => 33
+//"33abc" => NaN
+// true => 1 ; false => 0;
+
+let isLoggedIn = "Harendra";
+
+let booleanInLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanInLoggedIn);
+
+// 1 => true
+// 0 => falsh
+// "Harendra" => true
+// "" => falsh
+
+
+let someNumber = 23
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+
+console.log(stringNumber);
